6ee79223088f005f75ea7d29771fc87b852b65fc
Martin Ve <martinvetsu@gmail.com>
2025-08-31 18:21:55 +0200

    added axios for get, chnaged from direct fetch

--- src/pages/ExchangeRateList.js
+++ src/pages/ExchangeRateList.js
@@ -1,7 +1,9 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 // const CS_URL = "/api"
-const CS_URL = "https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e"
+// const CS_URL = "https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e"
+const LOCAL_URL = "http://localhost:8080"
 
 const ExchangeRatelist = () => {
 
@@ -9,9 +11,11 @@ const ExchangeRatelist = () => {
 
     useEffect(() => {
         const fetchRates = async () => {
-            const response = await fetch(CS_URL)
-            const data = await response.json()
-            setRates(data)
+            const response = await axios.get(`${LOCAL_URL}/api?usedb=true`)
+            setRates(response.data)
+            // const response = await fetch(CS_URL)
+            // const data = await response.json()
+            // setRates(data)
         }
         fetchRates()
     }, [])
